62399485bae863aa3d3ca03696e5223783499579
sfcdota <sfcdota@gmail.com>
2021-05-09 17:59:53 +0300

    temp. realized some headers

--- srcs/Map.hpp
+++ srcs/Map.hpp
@@ -1,4 +1,71 @@
 #ifndef MAP_HPP
 #define MAP_HPP
 
+template<
+    class Key,
+    class T,
+    class Compare = std::less<Key>,
+    class Alloc = allocator<std::pair<const Key, T>
+    >
+class map {
+  typedef key_type Key;
+  typedef mapped_type T;
+  typedef value_type std::pair<const key_type, mapped_type>;
+  typedef key_compare std::less<key_type>
+  ////////////////////typedef value_compare nullptr;
+  typedef allocator_type allocator<value_type>;
+  typedef reference &value_type;
+  typedef const_reference &value_type;
+  typedef pointer *value_type;
+  typedef const_pointer const *value_type;
+  typedef iterator ft::iterator;
+  typedef const_iterator ft::iterator;
+  typedef reverse_iterator ft::iterator;
+  typedef const_reverse_iterator ft::iterator;
+  typedef difference_type ptrdiff_t;
+  typedef size_type size_t;
+ public:
+    explicit map(const key_compare& comp = key_compare(),
+                 const allocator_type& alloc = allocator_type());
+    template <class InputIterator>
+        map (InputIterator fisrt, InputIterator last,
+             const key_compare& comp = key_compare(),
+             const allocator_type& alloc = allocator_type());
+    map(cosnt map& x);
+    ~map();
+    map& operator=(const map& x);
+    iterator begin();
+    const_iterator begin() const;
+    iterator end();
+    const_iterator end() const;
+    reverse_iterator rbegin();
+    const_reverse_iterator rbegin() const;
+    reverse_iterator rend();
+    const_reverse_iterator rend() const;
+    bool empty() const;
+    size_type size() const;
+    size_type max_size() const;
+    mapped_type& operator[] (const key_type& k);
+    pair<iterator, bool> insert (const value_type& val);
+    iterator insert (iterator position, const value_type& val);
+    template <class InputIterator>
+        void insert (InputIterator first, InputIterator last);
+    void erase(iterator position);
+    size_type erase(const key_type& k);
+    void erase(iterator first, iterator last);
+    void swap(map& xx);
+    void clear();
+    key_compare key_comp() const;
+    value_compare value_comp() const;
+    iterator find(const key_type& k);
+    const_iterator find(const key_type& k) const;
+    size_type count (const key_type& k) const;
+    iterator lower_bound (const key_type& k);
+    const_iterator lower_bound (const key_type& k) const;
+    iterator upper_bound (const key_type& k);
+    const_iterator upper_bound (const key_type& k) const;
+    pair<const_iterator, const_iterator> equal_range (const key_Type& k) const;
+    pair<iterator, iterator> equal_range (const key_type& k);
+ private:
+};
 #endif
--- srcs/Vector.hpp
+++ srcs/Vector.hpp
@@ -5,11 +5,61 @@ template <class T, class Alloc = allocator<T>>
 class vector {
   typedef value_type T;
   typedef allocator_type allocator<value_type>;
-  typedef reference value_type&;
-  typedef const_reference const value_type&;
-
+  typedef reference &value_type;
+  typedef const_reference const &value_type;
+  typedef pointer *value_type;
+  typedef const_pointer const *value_type;
+  typedef iterator ft::iterator<value_type>;
+  typedef const_iterator const ft::iterator<value_type> const;
+  typedef reverse_iterator ft::iterator<value_type>;
+  typedef const_reverse_iterator ft::iterator<value_type> const;
+  typedef difference_type ptrdiff_t;
+  typedef size_type size_t;
  public:
-
+  explicit vector(const allocator_type& alloc = allocator_type());
+  explicit vector(size_type n, const value_type& val = value_type(),
+                  const allocator_type& alloc = allocator_type());
+  template<class InputIterator>
+      vector(InputIterator first, InputIterator last,
+             const allocator_type& alloc = allocator_type());
+  vector(const vector& x);
+  ~vector();
+  vector& operator=(const vector&x);
+  iterator begin();
+  const_iterator begin() const;
+  iterator end();
+  const_iterator end() const;
+  reverse_iterator rbegin();
+  const_reverse_iterator rbegin() const;
+  reverse_iterator rend();
+  const_reverse_iterator rend() const;
+  size_type size() const;
+  size_type max_size const;
+  void resize(size_type n, value_type val = value_type());
+  size_type capacity() const;
+  bool empty() const;
+  void reverse(size_type n);
+  void shrink_to_fit();
+  reference operator[](size_type n);
+  const_reference operator[](size_type n) const;
+  reference at(size_type n);
+  const_reference at(size_type n) const;
+  reference front();
+  const_reference front() const;
+  reference back();
+  const_reference back() const;
+  template<class InputIterator>
+      void assign(InputIterator first, InputIterator last);
+  void assign(size_type n, const value_type& val);
+  void push_back (const value_type& val);
+  void pop_back();
+  iterator insert(iterator position, const value_type& val);
+  void insert(iterator position, size_type n, const value_type& val);
+  template<class InputIterator>
+      void insert(iterator position, InputIterator first, InputIterator last);
+  iterator erase(iterator position);
+  iterator erase(iterator first, iterator last);
+  void clear();
  private:
 };
 #endif
