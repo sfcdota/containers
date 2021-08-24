d84851b47612be32989f4646efd9099008d8cc55
sfcdota <sfcdota@gmail.com>
2021-08-24 13:33:38 +0300

    added diff to test script

--- test.sh
+++ test.sh
@@ -2,11 +2,13 @@ make fclean
 make FT=1 all
 echo "test ft containers with running 10000 times"
 make clean
-time ./containers 1000 > /dev/null
+time ./containers 1000 > ft_result
 make fclean
 
 make STD=1 all
 echo "test std containers with running 10000 times"
 make clean
-time ./containers 1000 > /dev/null
+time ./containers 1000 > std_result
 make fclean
+
+diff ft_result std_result && rm ft_result std_result
